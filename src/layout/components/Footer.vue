e0f07978f6aa86535efda37d44b3df37459e759a
gaoyang <1132477246@qq.com>
2023-05-31 13:05:18 +0800

    5.31

---
+++ src/layout/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <div class="footerBox">
+    <div class="container">
+      <div>
+        <div>
+          <div>
+            1
+          </div>
+          <div />
+          <div />
+
+        </div>
+        <div />
+
+      </div>
+    </div>
+
+  </div>
+
+</template>
+<script >
+
+export default {
+  name: 'Footer',
+
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
