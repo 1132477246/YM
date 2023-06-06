9876d822600fb9b1256b60798a6a3ef472746446
zhuyongqing <gameworld614@gmail.com>
2023-06-06 14:27:13 +0800

    save

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,8 @@ module.exports = {
       'viewportWidth': 1920,
       'unitPrecision': 5,
       'minPixelValue': 1,
+      fontViewportUnit: 'vw',
+      viewportUnit: 'vw',
       exclude: [/home_adapter.scss/, /navBar_adapter.scss/, /footer_adapter.scss/, /about-adapter.scss/, /partners-adapter.scss/]
     }
   }
